79a9570584cc4160678130493be61bbcaa24e5e8
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-13 21:00:43 +0530

    updated

---
+++ a4/script.js
@@ -0,0 +1,25 @@
+
+const daySelect = document.getElementById('dob-day');
+for (let i = 1; i <= 31; i++) {
+  daySelect.innerHTML += `<option value="${i}">${i}</option>`;
+}
+
+
+const monthSelect = document.getElementById('dob-month');
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+months.forEach((month, i) => {
+  monthSelect.innerHTML += `<option value="${i + 1}">${month}</option>`;
+});
+
+
+const yearSelect = document.getElementById('dob-year');
+const currentYear = new Date().getFullYear();
+for (let i = currentYear; i >= 1900; i--) {
+  yearSelect.innerHTML += `<option value="${i}">${i}</option>`;
+}
+
+
+
