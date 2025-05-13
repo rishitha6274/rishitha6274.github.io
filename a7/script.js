58cbc0c26bda1b192ee1b62b675d793d7dd9e287
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-13 19:53:59 +0530

    updated

--- a7/script.js
+++ a7/script.js
@@ -1,16 +1,15 @@
-function getresult() {
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a/b;
+const getresult = () => {
   const n1 = parseFloat(document.getElementById('n1').value);
   const n2 = parseFloat(document.getElementById('n2').value);
-  
-  const addition = n1 + n2;
-  const subtraction = n1 - n2;
-  const multiplication = n1 * n2;
-  const division = n1 / n2;
 
   document.getElementById('output').innerHTML = `
-        Addition: ${addition}<br>
-        Subtraction: ${subtraction}<br>
-        Multiplication: ${multiplication}<br>
-        Division: ${division}
-      `;
-    }
+    Addition: ${addition(n1, n2)}<br>
+    Subtraction: ${subtraction(n1, n2)}<br>
+    Multiplication: ${multiplication(n1, n2)}<br>
+    Division: ${division(n1, n2)}
+  `;
+};
