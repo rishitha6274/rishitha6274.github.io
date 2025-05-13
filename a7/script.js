d6643111c0169f7d77c36bfbaef0f631e94d278e
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-13 20:41:02 +0530

    updated

--- a7/script.js
+++ a7/script.js
@@ -1,15 +1,30 @@
+let resultHTML = ''; // Global variable to store result HTML
+
 const addition = (a, b) => a + b;
 const subtraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
-const division = (a, b) => a/b;
+const division = (a, b) => a / b;
+
 const getresult = () => {
   const n1 = parseFloat(document.getElementById('n1').value);
   const n2 = parseFloat(document.getElementById('n2').value);
 
-  document.getElementById('output').innerHTML = `
-    Addition: ${addition(n1, n2)}<br>
-    Subtraction: ${subtraction(n1, n2)}<br>
-    Multiplication: ${multiplication(n1, n2)}<br>
-    Division: ${division(n1, n2)}
-  `;
+   resultHTML = `
+      Addition: ${addition(n1, n2)}<br>
+      Subtraction: ${subtraction(n1, n2)}<br>
+      Multiplication: ${multiplication(n1, n2)}<br>
+      Division: ${division(n1, n2)}
+    `;
+  
+
+ 
+  document.getElementById('output').innerHTML = '';
+};
+
+const dispResult = () => {
+  document.getElementById('output').innerHTML = resultHTML;
+};
+
+const hideResult = () => {
+  document.getElementById('output').innerHTML = '';
 };
