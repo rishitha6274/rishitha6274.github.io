12fb6dc0d826380e3c5a4019b63b28f4cac33454
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-17 20:33:24 +0530

    updated

--- a13/index.js
+++ a13/index.js
@@ -1,16 +1,17 @@
 const products = [
-  { id: 1, name: "Product 1", price: 34, status:"pending" },
-  { id: 2, name: "Product 2", price: 56, status:"pending" },
-  { id: 3, name: "Product 3", price: 40, status:"pending" },
+  { id: 1, name: "Product 1", price: 34,qty:3, status:"pending" },
+  { id: 2, name: "Product 2", price: 56,qty:5, status:"pending" },
+  { id: 3, name: "Product 3", price: 40,qty:2, status:"pending" },
 ];
 
-const list = products.map(product => ({
+// const list = products.map(product => ({
   
-  ...product,
-  status: "completed",
-  price: product.price + 5,
-}));
+//   ...product,
+//   status: "completed",
+//   price: product.price + 5,
+// }));
 
+let filteredproduct = qty.filter((value) => value > 2);
 const totalPrice = list.reduce((sum, product) => sum + product.price, 0);
 
 list.forEach(product => {
