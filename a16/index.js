6db29c3b44e61c34f70f56d54a6c7142721f2501
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-17 20:46:22 +0530

    updated

---
+++ a16/index.js
@@ -0,0 +1,20 @@
+function add(x, y) {
+    return new Promise((resolve, reject) => {
+        resolve(x + y);
+        //reject("some error")
+    });
+}
+
+function sqr(a){
+    return a*a
+}
+async function main() {
+    try {
+        let sum = await add(4, 5);
+        let result = sqr(sum);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+main()
