95a06b93778b7205a19bcc93b5308286ab34e47f
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-18 18:21:34 +0530

    updated

---
+++ a19/index.js
@@ -0,0 +1,14 @@
+const products = [
+  { id: 1, name: "Product 1", price: 34,qty:3, status:"pending" },
+  { id: 2, name: "Product 2", price: 56,qty:5, status:"pending" },
+  { id: 3, name: "Product 3", price: 40,qty:2, status:"pending" },
+];
+
+const cart= {1:4,3:5}
+
+const total = products.reduce((sum, value)=>{
+   const qty = cart[value.id] ?? 0; 
+  return sum + value.price * qty;
+},0);
+
+console.log(total)
